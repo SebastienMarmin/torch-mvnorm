812a79a28d80fc07e7dacc6904a3f9ec71004d17
Sébastien Marmin <sebastien.marmin@lne.fr>
2021-05-19 16:12:34 +0200

    imprv doc

--- mvnorm/__init__.py
+++ mvnorm/__init__.py
@@ -1,4 +1,9 @@
-"""This modules documentation."""
+"""
+    - 1. Integrate multivariate normal density (CDFs)
+    - 2. Easily obtain partial derivatives of CDFs w.r.t location and covariance (and higher derivatives)
+    - 3. Manipulate quantities within a tensor-based framework (e.g. broadcasting is fully supported)
+
+"""
 
 from .multivariate_normal_cdf import multivariate_normal_cdf
 from .integration import integration
